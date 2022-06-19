848c5ace8bf533cc5a281063063c29d94fc8cba6
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-20 10:17:24 +1200

    started working on the Nav Component
    Signed-off-by: Heze <64218603+Hcmwebs@users.noreply.github.com>

--- src/JS/Components/Nav.js
+++ src/JS/Components/Nav.js
@@ -1,7 +1,15 @@
 import React from 'react'
+import logo from '../../Assets/logo-dark.svg'
 
 const Nav = () => {
-	return <nav className='nav'>nav component goes here</nav>
+	return (
+		<nav className='nav'>
+			<div className='img-container'>
+				<img src={logo} alt='logo' className='img'/>
+			</div>
+			<button className='btn btn-primary'>get started</button>
+		</nav>
+	)
 }
 
 export default Nav

968d3da05c70a9b4f6fa346031ecc5018cbf0e38
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-21 11:25:13 +1200

    started styling the hero section

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -3,7 +3,17 @@ import React from 'react'
 const Hero = () => {
 	return (
 		<div className='hero'>
-			<h1>hero component here</h1>
+			<div className='hero-text'>
+				<h1> Maximize skill, minimize budget</h1>
+				<p>
+					Our modern courses across a range of in-demand skills will give you
+					the knowledge you need to live the life you want.
+				</p>
+				<button className='btn btn-hero'> Get Started</button>
+			</div>
+			<div className='hero-img'>
+				<img src='' alt='' className='img' />
+			</div>
 		</div>
 	)
 }

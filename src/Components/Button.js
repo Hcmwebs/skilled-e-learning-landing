08fac8586c700aa795b84b5fa458923ce03cc7bd
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-20 13:58:24 +1200

    styled the nav

--- src/Components/Button.js
+++ src/Components/Button.js
@@ -1,9 +1,7 @@
 import React from 'react'
 
 const Button = () => {
-  return (
-    <button className="btn btn-primary">Button</button>
-  )
+	return <button className='btn'> Get Started</button>
 }
 
 export default Button
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import logo from '../Assets/logo-dark.svg'
+import Button from './Button'
 
 const Nav = () => {
 	return (
 		<nav className='nav'>
 			<div className='img-container'>
-				<img src={logo} alt='logo' className='img'/>
+				<img src={logo} alt='logo' className='img' />
 			</div>
-			<button className='btn btn-primary'>get started</button>
+			<button className='btn btn-nav'>Get started</button>
 		</nav>
 	)
 }

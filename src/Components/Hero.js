1bf8831d54ee4a47297610d62ed9c4cbc7cb75f1
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-22 01:09:49 +1200

    started styling the hero image

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,6 +1,18 @@
 import React from 'react'
-import heroMobilePng from '../Assets/image-hero-mobile.png';
+import heroMobilePng from '../Assets/image-hero-mobile.png'
 import heroMobileWebp from '../Assets/image-hero-mobile.webp'
+import heroMobile2xPng from '../Assets/image-hero-mobile@2x.png'
+import heroMobile2xWebp from '../Assets/image-hero-mobile@2x.webp'
+
+import heroTabletPng from '../Assets/image-hero-tablet.png'
+import heroTabletWebp from '../Assets/image-hero-tablet.webp'
+import heroTablet2xPng from '../Assets/image-hero-tablet@2x.png'
+import heroTablet2xWebp from '../Assets/image-hero-tablet@2x.webp'
+
+import heroDesktopPng from '../Assets/image-hero-desktop.png'
+import heroDesktopWebp from '../Assets/image-hero-desktop.webp'
+import heroDesktop2xPng from '../Assets/image-hero-desktop@2x.png'
+import heroDesktop2xWebp from '../Assets/image-hero-desktop@2x.webp'
 
 const Hero = () => {
 	return (
@@ -16,7 +28,17 @@ const Hero = () => {
 			<div className='hero-img'>
 				<div className='img-container'>
 					<picture>
-						<source  media ='(min-width: 768px)' srcSet ={heroMobilePng} type={heroMobilePng} />
+						<source
+							media='(min-width: 1440px)'
+							srcSet={`${heroDesktopPng}, ${heroDesktop2xPng},${heroDesktopWebp},${heroDesktop2xWebp}`}
+						/>
+						<source
+							media='(min-width: 768px)'
+							srcSet={`${heroTabletPng},${heroTablet2xPng},${heroTabletWebp},${heroTablet2xWebp}`}
+						/>
+						<source
+							srcSet={`${heroMobilePng},${heroMobile2xPng},${heroMobileWebp},${heroMobile2xWebp}`}
+						/>
 					</picture>
 					<img src={heroMobilePng} alt='hero' className='img' />
 				</div>

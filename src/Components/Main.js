44332631e41b3b2c8ae3ee40ca268a672fd12ce7
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-23 22:36:04 +1200

    added content to the main

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -3,8 +3,20 @@ import React from 'react'
 const Main = () => {
 	return (
 		<main className='main'>
-			<div className='container'>
-				<div>Main component goes here</div>
+			<div className='card card-one'>
+				<h3>Check out our most popular courses!</h3>
+			</div>
+			<div className='card '>
+				<h3>Check out our most popular courses!</h3>
+			</div>
+			<div className='card '>
+				<h3>Check out our most popular courses!</h3>
+			</div>
+			<div className='card'>
+				<h3>Check out our most popular courses!</h3>
+			</div>
+			<div className='card '>
+				<h3>Check out our most popular courses!</h3>
 			</div>
 		</main>
 	)

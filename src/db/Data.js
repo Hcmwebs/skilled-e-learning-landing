caf08fbfd2b41c18f0f969f398a26173b581ada6
Heze <64218603+Hcmwebs@users.noreply.github.com>
2022-06-24 22:47:43 +1200

    Finished styling main section for all the screens

--- src/db/Data.js
+++ src/db/Data.js
@@ -2,31 +2,31 @@ const cards = [
 	{
 		id: 2,
 		title: 'animation',
-		icon: '../Assets/icon-animation.svg',
+		icon: import('../Assets/icon-animation.svg'),
 		body: 'Learn the latest animation techniques to create stunning motion design and captivate your audience.',
 	},
 	{
 		id: 3,
 		title: 'Design',
-		icon: '../Assets/icon-design.svg',
+		icon: require('../Assets/icon-design.svg'),
 		body: 'Create beautiful, usable interfaces to help shape the future of how the web looks.',
 	},
 	{
 		id: 4,
 		title: 'Photography',
-		icon: '../Assets/icon-photography.svg',
+		icon: require('../Assets/icon-photography.svg'),
 		body: 'Explore critical fundamentals like lighting, composition, and focus to capture exceptional photos.',
 	},
 	{
 		id: 5,
 		title: 'Crypto',
-		icon: '../Assets/icon-crypto.svg',
+		icon: require('../Assets/icon-crypto.svg'),
 		body: 'All you need to know to get started investing in crypto. Go from beginner to advanced with this 54 hour course.',
 	},
 	{
 		id: 6,
 		title: 'Business',
-		icon: '../Assets/icon-business.svg',
+		icon: require('../Assets/icon-business.svg'),
 		body: 'A step-by-step playbook to help you start, scale, and sustain your business without outside investment',
 	},
 ]
